a9125d3d6b651a36abae5f207f4049112bf35bb1
Sandberg, Nathan <Nathan.Sandberg@cdk.com>
2022-08-05 11:14:41 -0500

    Updated to remove the @WebServlet in the Java files.

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -18,7 +18,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/helloworld/event")
 public class HelloWorld extends HttpServlet{
 
     public HelloWorld(){
